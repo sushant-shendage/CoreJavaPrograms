5021a96622b525524f41d519a293c5567f6ec217
Sushant Shendage <sushantshendage44@gmail.com>
2024-10-06 15:17:07 +0530

    javaPrimeNumberapproch1

---
+++ numberPgms/PrimeNumberApproch1.java
@@ -0,0 +1,22 @@
+class PrimeNumberApproch1
+{
+    public static void main(String[] args) 
+    {
+        int num=101,i;
+
+        for ( i = 2; i < num; i++) 
+        {
+            if (num%i==0) {
+                break;
+            }
+        }
+
+        if (num==i) {
+            System.out.println(num+" is a prime number");
+        }
+        else{
+            System.out.println(num+" is NOT a prime number");
+        }
+        
+    }
+}
