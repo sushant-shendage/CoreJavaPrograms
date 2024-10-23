18bac651774078db09f29939418da6469eaf49e7
Sushant Shendage <sushantshendage44@gmail.com>
2024-10-23 21:46:22 +0530

    update

--- strings/LeetCodepgms/CheckSubstring.java
+++ strings/LeetCodepgms/CheckSubstring.java
@@ -1,14 +1,14 @@
 public class CheckSubstring{
-
-       public int a=10;
-    public      {
-
-    }
+    
     public static void main(String[] args) {
-    // System.out.println(Check("leetcode", "leto"));
+    System.out.println(Check("leetcode", "le"));
+
+    // System.out.println("mainstring".indexOf("string"));
     }
     static int Check(String mainString,String subString){
-        if (mainString.indexOf(subString)>0) {
+     System.out.println(mainString.indexOf(subString));
+
+        if ((mainString.indexOf(subString))>=0) {
             return 1;
         }else{
             return -1;
