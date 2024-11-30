beec118e9e1617889ece6be9c7d20b3f6f985dba
Sushant Shendage <sushantshendage44@gmail.com>
2024-11-30 19:50:31 +0530

    update

---
+++ oops/inheritance_II/Driver1.java
@@ -0,0 +1,12 @@
+// package javapgms.oops.inheritance_II;
+
+public class Driver1 {
+    
+}
+
+class ParentA{
+    ParentA(){
+        System.out.println("ParentA no-arg constructor content..!");
+    }
+    
+}
