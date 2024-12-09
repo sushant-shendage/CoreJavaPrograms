a7b817466010d075d241a4e5e928d617f877dca4
Sushant Shendage <sushantshendage44@gmail.com>
2024-12-09 22:53:29 +0530

    update

--- multithreading/basics_01/Driver1.java
+++ multithreading/basics_01/Driver1.java
@@ -1,26 +1,46 @@
-package basics_01;
+// package basics_01;
 
 public class Driver1 {
     public static void main(String[] args) {
         
         MyThread1 t1=new MyThread1();
         MyThread2 t2=new MyThread2();
+        MyThread3 t3=new MyThread3();
+
 
         t1.start();
         t2.start();
+        t3.start();
+
 
     }
 }
 class MyThread1 extends Thread {
     @Override
     public void run(){
-System.out.println("thread 1 execution completed..!");
+        for (int i = 0; i <10; i++) {
+            
+            System.out.println("thread 1 execution completed..!");
+        }
     }
 }
 
 class MyThread2 extends Thread {
     @Override
     public void run(){
-        System.out.println("thread 2 execution completed..!");
+        for (int i = 0; i <10; i++) {
+            
+            System.out.println("thread 2 execution completed..!");
+        }
+    }
+}
+
+class MyThread3 extends Thread {
+    @Override
+    public void run(){
+        for (int i = 0; i <10; i++) {
+            
+            System.out.println("thread 3 execution completed..!");
+        }
     }
 }
--- multithreading/staticSynchronizedMethod_04/Driver1.java
+++ multithreading/staticSynchronizedMethod_04/Driver1.java
@@ -1,29 +1,58 @@
-package staticSynchronizedMethod_04;
+// package staticSynchronizedMethod_04;
 // package javapgms.multithreading.staticSynchronizedMethod;
 
 public class Driver1 {
     public static void main(String[] args) {
 
         MyThread t1=new MyThread();
-        t1.seat=(int)(Math.random()*100%10);
         t1.start();
+        
+        // MyThread t2=new MyThread();
+        // t2.start();
+        
+        while (BookMyShow.totalSeat!=0) {
+            try {
+                Thread.sleep(1000);
+                Thread.interrupted();
+            } catch (Exception e) {
+                // TODO: handle exception
+                System.out.println("exception occured..!");
+            }
+            t1.seat=(int)(Math.random()*100%10);
+            t1.run();
+            // t2.seat=(int)(Math.random()*100%10);
+            // t2.run();
+        }
 
-        MyThread t2=new MyThread();
-        t2.seat=(int)(Math.random()*100%10);
-        t2.start();
+       
     }
     
 }
 
 class BookMyShow{
-    static int totalSeat=10;
+    static int totalSeat=50;
     public static  synchronized void BookSeat(int seat){
         if (totalSeat>=seat) {
+            System.out.println("thread name :"+Thread.currentThread().getName());
             System.out.printf("\nbooked seats:: %d",seat);
             totalSeat-=seat;
             System.out.printf("\n seats left:: %d",totalSeat);
+            System.out.println();
+            for (int i = 1; i <=50; i++) {
+                if (i%10==0) {
+                    System.out.println();
+                }
+                if (i>=totalSeat) {
+                    System.out.print(" *");
+                } else {
+                    System.out.print(" -");
+                    
+                }
+            }
             System.out.println("\n============================================");
         } else {
+            System.out.println("thread name :"+Thread.currentThread().getName());
+
             System.out.printf("\n%d seats are not avilabe..!",seat);
             System.out.printf("\n seats left:: %d",totalSeat);
             System.out.printf("\n============================================");
