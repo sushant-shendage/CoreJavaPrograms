ed4983f3bb5c283742cf2089f749bb5efcee2a9b
Sushant Shendage <sushantshendage44@gmail.com>
2024-12-22 17:17:07 +0530

    update

---
+++ patterenPrograms/AllTrianglePatterens.java
@@ -0,0 +1,51 @@
+// package javapgms.patterenPrograms;
+
+public class AllTrianglePatterens  {
+    public static void main(String[] args) {
+         StarPatterenV1Sp0St1A01(7);
+         StarPatterenV1Sp0St1A02(7);
+         StarPatterenV1Sp1St1A01(7);
+ 
+    }
+
+    public static void StarPatterenV1Sp0St1A01(int numberOfRows) {
+        for (int i = 1; i <= numberOfRows; i++) {
+            for (int j = 1; j <=numberOfRows-i; j++) {
+                System.out.print("");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void StarPatterenV1Sp0St1A02(int numberOfRows) {
+        for (int i = 1; i <= numberOfRows; i++) {
+            for (int j = numberOfRows-i; j >0; j--) {
+                System.out.print("");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    public static void StarPatterenV1Sp1St1A01(int numberOfRows) {
+        for (int i = 1; i <= numberOfRows; i++) {
+            for (int j = 1; j <=numberOfRows-i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+}
+
+@FunctionalInterface
+interface FunctionalInterfaceForPattteren{
+    void patteren(int numberOfRows);
+}
