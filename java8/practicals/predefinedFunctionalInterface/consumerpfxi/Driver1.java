c8db1335e63382264a20311c636f8256eb3401ab
Sushant Shendage <sushantshendage44@gmail.com>
2024-12-28 16:35:56 +0530

    update

--- java8/practicals/predefinedFunctionalInterface/consumerpfxi/Driver1.java
+++ java8/practicals/predefinedFunctionalInterface/consumerpfxi/Driver1.java
@@ -1,11 +1,16 @@
 // package javapgms.java8.practicals.predefinedFunctionalInterface.consumerpfxi;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.function.Consumer;
 
 public class Driver1 {
     public static void main(String[] args) {
         Consumer <Integer> ob1=n->System.out.println(n);
         ob1.accept(546);
-    }
-    
+
+        List <Integer> l1=Arrays.asList(10,20,30,40);
+        l1.forEach(n->System.out.println(n));
+    }   
 }
