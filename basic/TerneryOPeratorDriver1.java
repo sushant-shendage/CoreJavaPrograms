6dd99f42f8c0bab0d6bf352fc6e7f6e03bebd6b1
Sushant Shendage <sushantshendage44@gmail.com>
2025-06-29 11:04:55 +0530

    update

---
+++ basic/TerneryOPeratorDriver1.java
@@ -0,0 +1,12 @@
+// package basic;
+
+public class TerneryOPeratorDriver1 {
+    public static void main(String[] args) {
+        int age = 67;
+
+        String status = (age >= 18) ? "allowed" : "NOT allowed";
+
+        System.out.println("You are " + ((age >= 18) ? "allowed" : "NOT allowed") + " for driving..!");
+
+    }
+}
